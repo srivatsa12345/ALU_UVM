ecff034738a5225fe034949f53aac2b7af4e1992
srivatsa12345 <srivatsasripathi@gmail.com>
2026-08-06 12:18:43 +0530

    Test cases Added

--- src/my_transaction.sv
+++ src/my_transaction.sv
@@ -8,6 +8,13 @@ class my_transaction extends uvm_sequence_item;
 	
 	logic rst;
 
+	static bit [5:0] count,delay_hold;
+	static bit [1:0] Inv_hold;
+	static bit m;
+	static bit [`CW-1:0] cmd_h;
+
+	rand bit [5:0] delay;
+
 	function new (string name="trans");
 		super.new(name);
 	endfunction
@@ -19,6 +26,7 @@ class my_transaction extends uvm_sequence_item;
 	constraint in_v{ INP_VALID inside {0,3};}
 	constraint ce{ CE inside {0,1};}
 	constraint cin{ CIN inside {0,1};}
+	constraint wait_cyc{ delay inside {[0:15]};}
 
 	`uvm_object_utils_begin(my_transaction)
 		`uvm_field_int(OPA, UVM_ALL_ON)
@@ -42,7 +50,7 @@ endclass
 class cont_arith_op extends my_transaction;
 	`uvm_object_utils(cont_arith_op)
 
-	function new (string name="con_op");
+	function new (string name="con_arith_op");
 		super.new(name);
 	endfunction
 		
@@ -55,25 +63,12 @@ endclass
 class cont_log_op extends my_transaction;
 	`uvm_object_utils(cont_log_op)
 
-	function new (string name="con_op");
+	function new (string name="con_log_op");
 		super.new(name);
 	endfunction
 		
 	constraint mode{ MODE==0;}
 	constraint cmd{ CMD inside {[0:13]};}
-	constraint in_v{ INP_VALID==3;}
-	constraint ce{ CE==1;}
-endclass
-
-class mul_op extends my_transaction;
-	`uvm_object_utils(mul_op)
-
-	function new (string name="mul_op");
-		super.new(name);
-	endfunction
-		
-	constraint mode{ MODE==1;}
-	constraint cmd{ CMD inside {9,10};}
 	constraint in_v{ INP_VALID==3;}
 	constraint ce{ CE==1;}
 endclass
@@ -92,6 +87,134 @@ class err_op extends my_transaction;
 	constraint ce{ CE==1;}
 endclass
 
+class mult_op extends my_transaction;
+	`uvm_object_utils(mult_op)
 
+	function new (string name="mult_op");
+		super.new(name);
+	endfunction
+		
+	constraint mode{ MODE==1;}
+	constraint cmd{ CMD inside {9,10};}
+	constraint in_v{ INP_VALID==3;}
+	constraint ce{ CE==1;}
+endclass
 
+class mul_during_op extends my_transaction;
+	`uvm_object_utils(mul_during_op)
+
+	function new (string name="mul_during_op");
+		super.new(name);
+	endfunction
+		
+	constraint mode{ MODE==1;}
+	constraint cmd{ CMD dist{[9:10]:=100, [0:8]:/50, [11:15]:/50};}
+	constraint in_v{ INP_VALID==3;}
+	constraint ce{ CE==1;}
+endclass
+
+class stop_ce_dur_op extends my_transaction;
+	`uvm_object_utils(stop_ce_dur_op)
+
+	function new (string name="stop_ce_dur_op");
+		super.new(name);
+	endfunction
+		
+	constraint ce{ CE dist{1:=1, 0:=4};}
+endclass
+
+class wait_rand_cycles extends my_transaction;
+	`uvm_object_utils(wait_rand_cycles)
+
+	function new (string name="wait_rand_cycles");
+		super.new(name);
+	endfunction
+
+	constraint wait_cyc{ delay inside {[0:14]};}
+	constraint in_v{ INP_VALID inside {[0:2]};}
+
+	function void post_randomize();
+		if ((m!=MODE)&&(cmd_h!=CMD)) begin
+			delay_hold=delay;
+			count=1;
+			Inv_hold=INP_VALID;	
+			m=MODE;
+			cmd_h=CMD;
+		end else if ((INP_VALID!=0)&&(count==0)) begin
+			delay_hold=delay;
+			count++;
+			Inv_hold=INP_VALID;
+			m=MODE;
+			cmd_h=CMD;
+		end else if (count==delay_hold) begin
+			count=0;
+			INP_VALID=~(Inv_hold);
+		end else if (count!=0) begin
+			count++;
+			INP_VALID=Inv_hold;
+		end
+	endfunction	
+endclass
+
+class wait_rand_cycles_w_mcmd extends my_transaction;
+	`uvm_object_utils(wait_rand_cycles_w_mcmd)
+
+	function new (string name="wait_rand_cycles_w_mcmd");
+		super.new(name);
+	endfunction
+
+	constraint wait_cyc{ delay inside {[0:14]};}
+	constraint in_v{ INP_VALID inside {[0:2]};}
+
+	function void post_randomize();
+		if ((INP_VALID!=0)&&(count==0)) begin
+			delay_hold=delay;
+			count++;
+			Inv_hold=INP_VALID;
+			m=MODE;
+			cmd_h=CMD;
+		end else if (count==delay_hold) begin
+			count=0;
+			INP_VALID=~(Inv_hold);
+			MODE=m;
+			CMD=cmd_h;
+		end else if (count!=0) begin
+			count++;
+			INP_VALID=Inv_hold;
+			MODE=m;
+			CMD=cmd_h;
+		end
+	endfunction	
+endclass
+
+class wait_rand_cycles_w_mcmd_16 extends my_transaction;
+	`uvm_object_utils(wait_rand_cycles_w_mcmd_16)
+
+	function new (string name="wait_rand_cycles_w_mcmd_16");
+		super.new(name);
+	endfunction
+
+	constraint wait_cyc{ delay==15;}
+	constraint in_v{ INP_VALID inside {[0:2]};}
+
+	function void post_randomize();
+		if ((INP_VALID!=0)&&(count==0)) begin
+			delay_hold=delay;
+			count++;
+			Inv_hold=INP_VALID;
+			m=MODE;
+			cmd_h=CMD;
+		end else if (count==delay_hold) begin
+			count=0;
+			INP_VALID=~(Inv_hold);
+			MODE=m;
+			CMD=cmd_h;
+		end else if (count!=0) begin
+			count++;
+			INP_VALID=Inv_hold;
+			MODE=m;
+			CMD=cmd_h;
+		end
+	endfunction	
+endclass
 
--- src/test.sv
+++ src/test.sv
@@ -1,6 +1,7 @@
 class test extends uvm_test;
 	`uvm_component_utils(test);
 	environment env;
+	sequences seq;
 
 	function new (string name,uvm_component parent);
 		super.new(name,parent);
@@ -8,18 +9,37 @@ class test extends uvm_test;
 
 	function void build_phase(uvm_phase phase);
 		super.build_phase(phase);
-		my_transaction::type_id::set_type_override(cont_log_op::get_type());
 		env=environment::type_id::create("env",this);
 	endfunction
 
 	task run_phase(uvm_phase phase);
-		sequences seq;
 		phase.raise_objection(this);
-		seq=sequences::type_id::create("seq");
-		seq.start(env.a_ag.sqr);
+		my_transaction::type_id::set_type_override(cont_arith_op::get_type());
+		run();
+		my_transaction::type_id::set_type_override(cont_log_op::get_type());
+		run();
+		my_transaction::type_id::set_type_override(err_op::get_type());
+		run();
+		my_transaction::type_id::set_type_override(mult_op::get_type());
+		run();
+		my_transaction::type_id::set_type_override(mul_during_op::get_type());
+		run();
+		my_transaction::type_id::set_type_override(stop_ce_dur_op::get_type());
+		run();
+		my_transaction::type_id::set_type_override(wait_rand_cycles::get_type());
+		run();
+		my_transaction::type_id::set_type_override(wait_rand_cycles_w_mcmd::get_type());
+		run();
+		my_transaction::type_id::set_type_override(wait_rand_cycles_w_mcmd_16::get_type());
+		run();
 		#20;
 		env.sc.compare_results();
 		phase.drop_objection(this);
+	endtask
+
+	task run();
+		seq=sequences::type_id::create("seq");
+		seq.start(env.a_ag.sqr);
 	endtask
 endclass
 
